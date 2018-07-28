a9a7be472eb71610cbe00537dd92d71822b10edd
TDang <tin2dang@gmail.com>
2018-07-28 16:12:28 -0500

    created number generators for the crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,11 +9,32 @@
 //Set up variables
 var win = 0;
 var lose = 0;
-var currentscore = 0;
-var targetscore = 0;
+var currentScore = 0;
+var targetScore = 0; // This is the score they need to match
 
+// assign random numbers to each crystal from 1-12
+var blue = (Math.floor(Math.random() * 12) + 1);
+var clear = (Math.floor(Math.random() * 12) + 1);
+var yellow = (Math.floor(Math.random() * 12) + 1);
+var green = (Math.floor(Math.random() * 12) + 1);
+
+// Set up target score from 19-120
+targetScore = (Math.floor(Math.random() * 102) + 19);
 
 function reset() {
+    //clear current score back to 0
     currentscore = 0;
-    targetscore = 0;
+
+    //Set up new crystal numbers
+    blue = (Math.floor(Math.random() * 12) + 1);
+    clear = (Math.floor(Math.random() * 12) + 1);
+    yellow = (Math.floor(Math.random() * 12) + 1);
+    green = (Math.floor(Math.random() * 12) + 1);
+
+    // Set up new target score
+    targetScore = (Math.floor(Math.random() * 102) + 19);
 }
+
+
+//They win if they match targetScore === winScore
+//They lose if they go over winScore; 
