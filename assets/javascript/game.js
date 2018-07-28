150c9ad4430214695f19685fac6c9d3bf43f155f
TDang <tin2dang@gmail.com>
2018-07-28 15:53:04 -0500

    set up variables in JS and placed pictures in html

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//Set up 4 crystals to click on
+//set up score counter - will be between 19-120
+//set up random number genaator for numbers between 1-12
+// setup click function for the 
+// Setup reset function to change the score counter and crystal #s
+
+
+
+//Set up variables
+var win = 0;
+var lose = 0;
+var currentscore = 0;
+var targetscore = 0;
+
+
+function reset() {
+    currentscore = 0;
+    targetscore = 0;
+}
